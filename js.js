11b20c3d9d05be8ac0f9a322540f074371e10860
Virendra <virendrapal10081996@gmail.com>
2023-11-22 14:37:52 +0530

    scroll

---
+++ js.js
@@ -0,0 +1,54 @@
+let ready = false;
+const count = 20;
+let imagesLoaded = 0;
+const apikey = "sU3qOhPWWNqiyUkbEkJlhQ4WwasrPvadWyBcHZCF9C0";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apikey}&count=${count}&page=2`;
+
+const imagesContainer = document.getElementById("imagesContainer");
+const loader = document.getElementById("loader");
+console.log(loader)
+const getImages = async () => {
+  try {
+    loader.style.display = "block";
+    const response = await fetch(apiUrl);
+    const imagesArray = await response.json();
+    displayImages(imagesArray);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const onImageLoaded = () => {
+  imagesLoaded++;
+  if (count === imagesLoaded) {
+    // All the images have loaded
+    ready = true; // Ready to load more images
+    loader.style.display = "none";
+    console.log("READY IS BEING SET TO TRUE");
+  }
+  console.log("Img finished loading");
+};
+
+const displayImages = (imagesArray) => {
+  imagesArray.forEach((imageObject) => {
+    const img = document.createElement("img");
+    img.src = imageObject.urls.regular;
+    img.alt = "Random Image";
+    img.addEventListener("load", onImageLoaded);
+    imagesContainer.append(img);
+  });
+};
+
+getImages();
+
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY + window.innerHeight >= document.body.offsetHeight &&
+    ready === true
+  ) {
+    ready = false;
+    imagesLoaded = 0;
+    getImages();
+    console.log("Reached bottom");
+  }
+});
